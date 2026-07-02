fed19f5aea71274d65ace9da799b61ef6d0eaa4c
Alexanderzsj <shengjie24@mails.jlu.edu.cn>
2026-07-02 09:28:59 +0800

    Update Loss.py

--- utils/Loss.py
+++ utils/Loss.py
@@ -2,6 +2,43 @@ import warnings
 import torch.nn.functional as F
 
 
+def get_loss(model_name, model_out, label, args=None):
+    if model_name == 'S4Mamba':
+        loss_func = torch.nn.CrossEntropyLoss()
+        if isinstance(model_out, (tuple, list)):
+            logits1, logit2 = model_out
+            ls = loss_func(logits1, label) + loss_func(logit2, label) * 0.1
+        else:
+            ls = loss_func(model_out, label)
+            
+        return ls
+    
+    else:
+        # 其他基线模型的默认处理
+        loss_func = torch.nn.CrossEntropyLoss()
+        ls = loss_func(model_out, label)
+
+    return ls
+
+
+def get_pred(net, model_name, model_type_flag, batch_data, device):
+    if model_type_flag == 1:
+        X = batch_data.to(device)
+        out = net(X)
+    else:
+        print("model_type_flag error! ERROR in utils/loss.py")
+        exit()
+
+    if isinstance(out, dict) and 'logits' in out:
+        out = out['logits']
+        
+    elif isinstance(out, (tuple, list)):
+        out = out[-1] 
+
+    pre_y = out.cpu().argmax(axis=1).detach().numpy()
+    return pre_y
+
+
 def resize(input,
            size=None,
            scale_factor=None,
